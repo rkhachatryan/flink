12ad89f467900ef66e6adc1759e31d1711dd945c
Roman Khachatryan <khachatryan.roman@gmail.com>
2020-10-06 20:38:45 +0200

    [FLINK-19503][dstl] Add DSTL module and interfaces

---
+++ flink-state-backends/flink-statebackend-dstl/src/main/java/org/apache/flink/dstl/LogRecord.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.dstl;
+
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.util.Preconditions;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Representation of a state change or an output element.
+ * Interpretation depends on the state backend.
+ */
+@Internal
+public interface LogRecord {
+
+	int keyGroup();
+
+	long timestamp();
+
+	byte[] key();
+
+	byte[] value();
+
+	/**
+	 * Generic {@link LogRecord}.
+	 */
+	class GenericLogRecord implements LogRecord {
+
+		private final int keyGroup;
+		private final long timestamp;
+		private final byte[] key;
+		private final byte[] value;
+
+		GenericLogRecord(int keyGroup, long timestamp, byte[] key, byte[] value) {
+			Preconditions.checkArgument(keyGroup >= 0);
+			Preconditions.checkArgument(timestamp >= 0);
+			this.keyGroup = keyGroup;
+			this.timestamp = timestamp;
+			this.key = Preconditions.checkNotNull(key);
+			this.value = Preconditions.checkNotNull(value);
+		}
+
+		@Override
+		public int keyGroup() {
+			return keyGroup;
+		}
+
+		@Override
+		public long timestamp() {
+			return timestamp;
+		}
+
+		@Override
+		public byte[] key() {
+			return key;
+		}
+
+		@Override
+		public byte[] value() {
+			return value;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("keyGroup=%d, timestamp=%d, keyLen=%s, valueLen=%s", keyGroup, timestamp, key.length, value.length);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (!(o instanceof GenericLogRecord)) {
+				return false;
+			}
+			final GenericLogRecord that = (GenericLogRecord) o;
+			return keyGroup == that.keyGroup &&
+				timestamp == that.timestamp &&
+				Arrays.equals(key, that.key) &&
+				Arrays.equals(value, that.value);
+		}
+
+		@Override
+		public int hashCode() {
+			int result = Objects.hash(keyGroup, timestamp);
+			result = 31 * result + Arrays.hashCode(key);
+			result = 31 * result + Arrays.hashCode(value);
+			return result;
+		}
+	}
+
+	static LogRecord of(int keyGroup, long timestamp, byte[] key, byte[] value) {
+		return new GenericLogRecord(keyGroup, timestamp, key, value);
+	}
+}
---
+++ flink-state-backends/flink-statebackend-dstl/src/main/java/org/apache/flink/dstl/LogWriter.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.dstl;
+
+import org.apache.flink.annotation.Internal;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Allows to write data to the log.
+ * Scoped to a single writer (e.g. state backend).
+ */
+@Internal
+public interface LogWriter {
+
+	/**
+	 * Unique ID of this log.
+	 */
+	LogId logId();
+
+	/**
+	 * Appends the provided data to this log. No persistency guarantees.
+	 */
+	void append(int keyGroup, byte[] key, byte[] value, long timestamp);
+
+	/**
+	 * Get {@link SequenceNumber} of the last element added by {@link #append(int, byte[], byte[], long) append}.
+	 */
+	SequenceNumber lastAppendedSqn();
+
+	/**
+	 * Durably store previously {@link #append(int, byte[], byte[], long) appended} data.
+	 * @param after inclusive
+	 * @param until exclusive
+	 */
+	CompletableFuture<LogPointer> persistUntil(SequenceNumber after, SequenceNumber until);
+
+	/**
+	 * Close this log.
+	 * No new appends will be possible. Any appended but not persisted records will be lost.
+	 */
+	void close();
+}
