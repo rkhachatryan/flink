austince/flink-ci-flink

[FLINK-21355][state/changelog] Connect backend to writer factory

flink-runtime/src/main/java/org/apache/flink/runtime/state/StateBackendLoader.java
@@ -22,6 +22,8 @@ import org.apache.flink.configuration.CheckpointingOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.IllegalConfigurationException;
 import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.runtime.state.changelog.StateChangelogWriterFactory;
+import org.apache.flink.runtime.state.changelog.inmemory.InMemoryStateChangelogWriterFactory;
 import org.apache.flink.runtime.state.delegate.DelegatingStateBackend;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackendFactory;
@@ -395,8 +397,8 @@ public class StateBackendLoader {
             Constructor<? extends DelegatingStateBackend> constructor =
                     Class.forName(CHANGELOG_STATE_BACKEND, false, classLoader)
                             .asSubclass(DelegatingStateBackend.class)
-                            .getConstructor(StateBackend.class);
-            return constructor.newInstance(backend);
+                            .getConstructor(StateBackend.class, StateChangelogWriterFactory.class);
+            return constructor.newInstance(backend, new InMemoryStateChangelogWriterFactory());
         } catch (ClassNotFoundException e) {
             throw new DynamicCodeLoadingException(
                     "Cannot find DelegateStateBackend class: " + CHANGELOG_STATE_BACKEND, e);

flink-state-backends/flink-statebackend-changelog/src/main/java/org/apache/flink/state/changelog/ChangelogStateBackend.java
@@ -34,6 +34,7 @@ import org.apache.flink.runtime.state.KeyedStateHandle;
 import org.apache.flink.runtime.state.OperatorStateBackend;
 import org.apache.flink.runtime.state.OperatorStateHandle;
 import org.apache.flink.runtime.state.StateBackend;
+import org.apache.flink.runtime.state.changelog.StateChangelogWriterFactory;
 import org.apache.flink.runtime.state.delegate.DelegatingStateBackend;
 import org.apache.flink.runtime.state.ttl.TtlTimeProvider;
 import org.apache.flink.util.Preconditions;
@@ -51,9 +52,12 @@ public class ChangelogStateBackend implements DelegatingStateBackend, Configurab
     private static final long serialVersionUID = 1000L;
 
     private final StateBackend delegatedStateBackend;
+    private final StateChangelogWriterFactory<?> stateChangelogWriterFactory;
 
-    public ChangelogStateBackend(StateBackend stateBackend) {
+    public ChangelogStateBackend(
+            StateBackend stateBackend, StateChangelogWriterFactory<?> stateChangelogWriterFactory) {
         this.delegatedStateBackend = Preconditions.checkNotNull(stateBackend);
+        this.stateChangelogWriterFactory = stateChangelogWriterFactory;
 
         Preconditions.checkArgument(
                 !(stateBackend instanceof ChangelogStateBackend),
@@ -155,7 +159,8 @@ public class ChangelogStateBackend implements DelegatingStateBackend, Configurab
         if (delegatedStateBackend instanceof ConfigurableStateBackend) {
             return new ChangelogStateBackend(
                     ((ConfigurableStateBackend) delegatedStateBackend)
-                            .configure(config, classLoader));
+                            .configure(config, classLoader),
+                    this.stateChangelogWriterFactory);
         }
 
         return this;

flink-state-backends/flink-statebackend-changelog/src/test/java/org/apache/flink/state/changelog/ChangelogDelegateStateTest.java
@@ -31,6 +31,7 @@ import org.apache.flink.runtime.state.KeyedStateBackend;
 import org.apache.flink.runtime.state.StateBackendTestBase;
 import org.apache.flink.runtime.state.VoidNamespace;
 import org.apache.flink.runtime.state.VoidNamespaceSerializer;
+import org.apache.flink.runtime.state.changelog.inmemory.InMemoryStateChangelogWriterFactory;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.util.IOUtils;
 
@@ -113,7 +114,11 @@ public class ChangelogDelegateStateTest {
 
         try {
             delegatedBackend = createKeyedBackend(backend.get(), env);
-            changelogBackend = createKeyedBackend(new ChangelogStateBackend(backend.get()), env);
+            changelogBackend =
+                    createKeyedBackend(
+                            new ChangelogStateBackend(
+                                    backend.get(), new InMemoryStateChangelogWriterFactory()),
+                            env);
 
             State state =
                     changelogBackend.getPartitionedState(

rkhachatryan/flink

flink-runtime/src/main/java/org/apache/flink/runtime/state/StateBackendLoader.java
@@ -23,6 +23,8 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.IllegalConfigurationException;
 import org.apache.flink.configuration.ReadableConfig;
 import org.apache.flink.configuration.StateBackendOptions;
+import org.apache.flink.runtime.state.changelog.StateChangelogStorage;
+import org.apache.flink.runtime.state.changelog.inmemory.InMemoryStateChangelogStorage;
 import org.apache.flink.runtime.state.delegate.DelegatingStateBackend;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackendFactory;
@@ -392,8 +394,8 @@ public class StateBackendLoader {
             Constructor<? extends DelegatingStateBackend> constructor =
                     Class.forName(CHANGELOG_STATE_BACKEND, false, classLoader)
                             .asSubclass(DelegatingStateBackend.class)
-                            .getConstructor(StateBackend.class);
-            return constructor.newInstance(backend);
+                            .getConstructor(StateBackend.class, StateChangelogStorage.class);
+            return constructor.newInstance(backend, new InMemoryStateChangelogStorage());
         } catch (ClassNotFoundException e) {
             throw new DynamicCodeLoadingException(
                     "Cannot find DelegateStateBackend class: " + CHANGELOG_STATE_BACKEND, e);

flink-state-backends/flink-statebackend-changelog/src/main/java/org/apache/flink/state/changelog/ChangelogStateBackend.java
@@ -35,6 +35,7 @@ import org.apache.flink.runtime.state.OperatorStateBackend;
 import org.apache.flink.runtime.state.OperatorStateHandle;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.changelog.ChangelogStateBackendHandle;
+import org.apache.flink.runtime.state.changelog.StateChangelogStorage;
 import org.apache.flink.runtime.state.changelog.inmemory.InMemoryStateChangelogStorage;
 import org.apache.flink.runtime.state.delegate.DelegatingStateBackend;
 import org.apache.flink.runtime.state.ttl.TtlTimeProvider;
@@ -61,9 +62,12 @@ public class ChangelogStateBackend implements DelegatingStateBackend, Configurab
     private static final Logger LOG = LoggerFactory.getLogger(ChangelogStateBackend.class);
 
     private final StateBackend delegatedStateBackend;
+    private final StateChangelogStorage<?> stateChangelogWriterFactory;
 
-    public ChangelogStateBackend(StateBackend stateBackend) {
+    public ChangelogStateBackend(
+            StateBackend stateBackend, StateChangelogStorage<?> stateChangelogWriterFactory) {
         this.delegatedStateBackend = Preconditions.checkNotNull(stateBackend);
+        this.stateChangelogWriterFactory = stateChangelogWriterFactory;
 
         Preconditions.checkArgument(
                 !(stateBackend instanceof DelegatingStateBackend),
@@ -176,7 +180,8 @@ public class ChangelogStateBackend implements DelegatingStateBackend, Configurab
         if (delegatedStateBackend instanceof ConfigurableStateBackend) {
             return new ChangelogStateBackend(
                     ((ConfigurableStateBackend) delegatedStateBackend)
-                            .configure(config, classLoader));
+                            .configure(config, classLoader),
+                    this.stateChangelogWriterFactory);
         }
 
         return this;

flink-state-backends/flink-statebackend-changelog/src/test/java/org/apache/flink/state/changelog/ChangelogDelegateStateTest.java
@@ -31,6 +31,7 @@ import org.apache.flink.runtime.state.KeyedStateBackend;
 import org.apache.flink.runtime.state.StateBackendTestBase;
 import org.apache.flink.runtime.state.VoidNamespace;
 import org.apache.flink.runtime.state.VoidNamespaceSerializer;
+import org.apache.flink.runtime.state.changelog.inmemory.InMemoryStateChangelogStorage;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.util.IOUtils;
 
@@ -113,7 +114,11 @@ public class ChangelogDelegateStateTest {
 
         try {
             delegatedBackend = createKeyedBackend(backend.get(), env);
-            changelogBackend = createKeyedBackend(new ChangelogStateBackend(backend.get()), env);
+            changelogBackend =
+                    createKeyedBackend(
+                            new ChangelogStateBackend(
+                                    backend.get(), new InMemoryStateChangelogStorage()),
+                            env);
 
             State state =
                     changelogBackend.getPartitionedState(