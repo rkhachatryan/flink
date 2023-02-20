125fa8e28b3c56cd56d025f124cfd337922221c6
wangfeifan <zoltar9264@163.com>
2023-02-20 20:26:58 +0800

    [FLINK-31139][state/changelog] not upload empty state changelog file

--- flink-dstl/flink-dstl-dfs/src/main/java/org/apache/flink/changelog/fs/BatchingStateChangeUploadScheduler.java
+++ flink-dstl/flink-dstl-dfs/src/main/java/org/apache/flink/changelog/fs/BatchingStateChangeUploadScheduler.java
@@ -220,6 +220,11 @@ class BatchingStateChangeUploadScheduler implements StateChangeUploadScheduler {
             scheduledBytesCounter = 0;
             scheduledFuture = null;
         }
+
+        if (tasks.size() == 0) {
+            return;
+        }
+
         try {
             Throwable error = getErrorSafe();
             if (error != null) {
